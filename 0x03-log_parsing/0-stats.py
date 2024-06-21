808b147726c4b9e9a8e83a4a8bcaa16657146df7
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-06-21 04:43:05 +0300

    RegEx

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,37 @@
 #!/usr/bin/python3
+'''a script that reads stdin line by line and computes metrics'''
 
-
-"""
-This module holds a simple module to do log
-parsing. Given a input of log files, extract some
-usefull information and print it to standard output
-"""
 
 import sys
-import re
-def check_log_format(log_string):
-    regex_pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "GET \/projects\/260 HTTP\/1\.1" \d{3} \d+'
-    return re.match(regex_pattern, log_string)
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def print_to_stdout(**kwargs):
-    """
-    A simple helper function to print
-    the values to standard output
-    """
-    for key in kwargs.keys():
-        if not isinstance(kwargs.get(key), dict):
-            # Means this is not a dictionary of dictionaries
-            # So Its the file size
-            print("File Size {}".format(kwargs.get(key)))
-        else:
-            for nested_k, nested_v in key.items():
-                print(f"{nested_k}: {nested_v}")
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def read_and_analyze_log_files():
-    while True:
-        try:
-            status_codes = {
-                "200": 0,
-                "301": 0,
-                "400": 0,
-                "401": 0,
-                "403": 0,
-                "404": 0,
-                "405": 0,
-                "500": 0
+except Exception as err:
+    pass
 
-            }
-            total_size = 0
-            for time in range(11):
-                log_info = input("").strip()
-                if check_log_format(log_info):
-                    log_info_list = log_info.split(" ")
-                    f_size = log_info_list[-1]
-                    s_code = log_info_list[-2]
-                    status_codes[s_code] = status_codes.get(s_code) + 1
-                    total_size += int(f_size)
-                    sorted_keys_dict = {code: status_codes[code] for code in sorted(status_codes)}
-            print_to_stdout(codes=sorted_keys_dict, size = total_size)
-        except KeyboardInterrupt:
-            print_to_stdout(codes=sorted_keys_dict, size = total_size)
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
