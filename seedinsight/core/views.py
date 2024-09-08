73d15123fd3580b4d987f0c174d1b7a6e2f8fc60
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-09-08 10:24:00 -0700

    Views

--- seedinsight/core/views.py
+++ seedinsight/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from .models import Seed, Ecozone
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def seed_recommendation(request):
+    seeds = None
+    if request.method == 'POST':
+        ecozone_id = request.POST.get('ecozone')
+        ecozone = Ecozone.objects.get(id=ecozone_id)
+        seeds = Seed.objects.filter(ecozone=ecozone)
+    return render(request, 'core/recommendation.html', {'seeds': seeds})
+
