0e26ffeffc7e842f64cac65f960aa5705bfb840e
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-09-08 10:31:08 -0700

    DB

--- seedinsight/seedinsight/settings.py
+++ seedinsight/seedinsight/settings.py
@@ -78,7 +78,11 @@ WSGI_APPLICATION = 'seedinsight.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'seedinsight_db',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
